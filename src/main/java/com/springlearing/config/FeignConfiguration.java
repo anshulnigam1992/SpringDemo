186639424af1f1e42e82532613197a465adf7ed6
Anshul Nigam <Anshul.Nigam@veritis.com>
2019-05-21 11:03:05 +0530

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/com/springlearing/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.springlearing.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.springlearing")
+public class FeignConfiguration {
+
+}
---
+++ src/main/java/com/springlearing/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.springlearing.config.audit;
